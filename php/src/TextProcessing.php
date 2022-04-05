4540be2908127613908aa01f4b27400fd10e1ff0
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-05 16:27:36 +0200

    refactor regex for more semantic

--- php/src/TextProcessing.php
+++ php/src/TextProcessing.php
@@ -4,9 +4,11 @@ namespace TextProcessingKata;
 
 class TextProcessing implements Processor
 {
+    private const CLEANER_SIGNS_REGEX = '/[^A-Za-z ]/';
+
     public function analyse(string $text): string
     {
-        return preg_replace("/[^A-Za-z ]/", '', strtolower($text));
+        return preg_replace(self::CLEANER_SIGNS_REGEX, '', strtolower($text));
     }
 
     public function orderText(string $text): array
