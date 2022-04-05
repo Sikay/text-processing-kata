05e50397e8b54de16c07d4bdee96719316c3ebfe
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-05 14:11:13 +0200

    WIP: should_return_the_most_repeated_word_2

--- php/src/TextProcessing.php
+++ php/src/TextProcessing.php
@@ -6,6 +6,12 @@ class TextProcessing implements Processor
 {
     public function analyse(string $text): string
     {
-        return 'you';
+        $wordsInText = array_count_values(explode(" ", strtolower($text)));
+        arsort($wordsInText);
+        $wordsOrder = [];
+        foreach ($wordsInText as $word => $repetitions) {
+            array_push($wordsOrder, $word);
+        }
+        return $wordsOrder[0];
     }
 }
--- php/tests/TextProcessingTest.php
+++ php/tests/TextProcessingTest.php
@@ -16,4 +16,14 @@ class TextProcessingTest extends TestCase
 
         self::assertTrue($mostRepeatedWord === 'you');
     }
+
+    /** @test */
+    public function should_return_the_most_repeated_word_2(): void
+    {
+        $text = 'Hello, this. is an example for you to practice. You, should grab this text, and make it as your test case.';
+        $textProcessor = new TextProcessing();
+        $mostRepeatedWord = $textProcessor->analyse($text);
+
+        self::assertTrue($mostRepeatedWord === 'you');
+    }
 }
