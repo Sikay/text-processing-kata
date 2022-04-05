6a9456c3d4e6e6c85df142bed417a259a84311a7
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-05 14:20:02 +0200

    WIP: should_cleaned_text

--- php/tests/TextProcessingTest.php
+++ php/tests/TextProcessingTest.php
@@ -12,7 +12,7 @@ class TextProcessingTest extends TestCase
     {
         $text = 'Hello, this is an example for you to practice. You should grab this text and make it as your test case.';
         $textProcessor = new TextProcessing();
-        $mostRepeatedWord = $textProcessor->analyse($text);
+        $mostRepeatedWord = $textProcessor->orderText($text);
 
         self::assertTrue($mostRepeatedWord === 'you');
     }
@@ -22,8 +22,19 @@ class TextProcessingTest extends TestCase
     {
         $text = 'Hello, this. is an example for you to practice. You, should grab this text, and make it as your test case.';
         $textProcessor = new TextProcessing();
-        $mostRepeatedWord = $textProcessor->analyse($text);
+        $mostRepeatedWord = $textProcessor->orderText($text);
 
         self::assertTrue($mostRepeatedWord === 'you');
+    }
+
+    /** @test */
+    public function should_cleaned_text(): void
+    {
+        $text = 'Hello, this. is an example for you to practice. You, should grab this text, and make it as your test case.';
+        $expectText = 'hello this is an example for you to practice you should grab this text and make it as your test case';
+        $textProcessor = new TextProcessing();
+        $cleanedText = $textProcessor->analyse($text);
+
+        self::assertTrue($cleanedText === $expectText);
     }
 }
