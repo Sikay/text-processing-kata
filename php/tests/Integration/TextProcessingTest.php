89034e47275e88cbf0b23f08c968ba4163a3ee76
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-05 16:23:37 +0200

    refactor test for more semantic

--- php/tests/Integration/TextProcessingTest.php
+++ php/tests/Integration/TextProcessingTest.php
@@ -7,25 +7,37 @@ use PHPUnit\Framework\TestCase;
 
 class TextProcessingTest extends TestCase
 {
-    /** @test */
-    public function should_return_the_most_repeated_word(): void
-    {
-        $text = 'Hello, this is an example for you to practice. You should grab this text and make it as your test case.';
-        $textProcessor = new TextProcessing();
-        $cleanedText = $textProcessor->analyse($text);
-        $mostRepeatedWord = $textProcessor->orderText($cleanedText);
+    protected $textProcessor;
 
-        self::assertTrue($mostRepeatedWord[0] === 'you');
+    private const VALID_TEXT = 'Hello, this is an example for you to practice. You should grab this text and make it as your test case.';
+    private const VALID_TEXT_WITH_MORE_SIGNS = 'Hello, this. is an example for you to practice. You, should grab this text, and make it as your test case.';
+    private const MOST_REPEATED_WORD = 'you';
+
+    protected function setUp(): void
+    {
+        $this->textProcessor = new TextProcessing();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->textProcessor = null;
     }
 
     /** @test */
-    public function should_return_the_most_repeated_word_2(): void
+    public function should_return_the_most_repeated_word(): void
     {
-        $text = 'Hello, this. is an example for you to practice. You, should grab this text, and make it as your test case.';
-        $textProcessor = new TextProcessing();
-        $cleanedText = $textProcessor->analyse($text);
-        $mostRepeatedWord = $textProcessor->orderText($cleanedText);
+        $cleanedText = $this->textProcessor->analyse(self::VALID_TEXT);
+        $orderWordsByRepeated = $this->textProcessor->orderText($cleanedText);
 
-        self::assertTrue($mostRepeatedWord[0] === 'you');
+        self::assertTrue($orderWordsByRepeated[0] === self::MOST_REPEATED_WORD);
+    }
+
+    /** @test */
+    public function should_return_the_most_repeated_word_with_more_signs(): void
+    {
+        $cleanedText = $this->textProcessor->analyse(self::VALID_TEXT_WITH_MORE_SIGNS);
+        $orderWordsByRepeated = $this->textProcessor->orderText($cleanedText);
+
+        self::assertTrue($orderWordsByRepeated[0] === self::MOST_REPEATED_WORD);
     }
 }
--- php/tests/Unit/TextProcessingTest.php
+++ php/tests/Unit/TextProcessingTest.php
@@ -7,24 +7,33 @@ use PHPUnit\Framework\TestCase;
 
 class TextProcessingTest extends TestCase
 {
+    protected $textProcessor;
+
+    private const VALID_TEXT = 'Hello, this. is an example for you to practice. You, should grab this text, and make it as your test case.';
+    private const VALID_CLEANED_TEXT = 'hello this is an example for you to practice you should grab this text and make it as your test case';
+    private const MOST_REPEATED_WORD = 'you';
+
+    protected function setUp(): void
+    {
+        $this->textProcessor = new TextProcessing();
+    }
+
+    protected function tearDown(): void
+    {
+        $this->textProcessor = null;
+    }
+
     /** @test */
     public function should_return_the_most_repeated_word(): void
     {
-        $text = 'hello this is an example for you to practice you should grab this text and make it as your test case';
-        $textProcessor = new TextProcessing();
-        $mostRepeatedWord = $textProcessor->orderText($text);
-
-        self::assertTrue($mostRepeatedWord[0] === 'you');
+        $orderWordsByRepeated = $this->textProcessor->orderText(self::VALID_CLEANED_TEXT);
+        self::assertTrue($orderWordsByRepeated[0] === self::MOST_REPEATED_WORD);
     }
 
     /** @test */
     public function should_cleaned_text(): void
     {
-        $text = 'Hello, this. is an example for you to practice. You, should grab this text, and make it as your test case.';
-        $expectText = 'hello this is an example for you to practice you should grab this text and make it as your test case';
-        $textProcessor = new TextProcessing();
-        $cleanedText = $textProcessor->analyse($text);
-
-        self::assertTrue($cleanedText === $expectText);
+        $cleanedText = $this->textProcessor->analyse(self::VALID_TEXT);
+        self::assertTrue($cleanedText === self::VALID_CLEANED_TEXT);
     }
 }
