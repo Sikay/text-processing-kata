98a2e275cea87968a0464b0d958fd538eb46e0a0
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-04-07 13:08:11 +0200

    refactor to const for more semantic

--- php/tests/Unit/TextProcessingTest.php
+++ php/tests/Unit/TextProcessingTest.php
@@ -8,6 +8,7 @@ use PHPUnit\Framework\TestCase;
 class TextProcessingTest extends TestCase
 {
     private const VALID_TEXT = 'Hello, this. is an example for you to practice. You, should grab this text, and make it as your test case.';
+    private const TOTAL_WORDS_IN_VALID_TEXT = 21;
     private const VALID_CLEANED_TEXT = 'hello this is an example for you to practice you should grab this text and make it as your test case';
     private const MOST_REPEATED_WORD = 'you';
     private const TEXT_WITH_CODE_SNIPPETS = 'Hello, this is an example for you to practice. You should grab this text and make it as your test case:
@@ -16,6 +17,8 @@ class TextProcessingTest extends TestCase
                                                 console.log("should should should")
                                                 }
                                                 ` ` `';
+    private const EMPTY_STRING = '';
+    private const INVALID_TEXT_WITH_ONLY_NUMBERS = '386491';
 
     /** @test */
     public function should_return_the_most_repeated_word(): void
@@ -43,20 +46,20 @@ class TextProcessingTest extends TestCase
     public function should_return_total_words(): void
     {
         $textProcessor = new TextProcessing(self::VALID_TEXT);
-        self::assertTrue($textProcessor->count() === 21);
+        self::assertTrue($textProcessor->count() === self::TOTAL_WORDS_IN_VALID_TEXT);
     }
 
     /** @test */
     public function should_fail_when_give_a_empty_string(): void
     {
         $this->expectException(\InvalidArgumentException::class);
-        $textProcessor = new TextProcessing('');
+        $textProcessor = new TextProcessing(self::EMPTY_STRING);
     }
 
     /** @test */
     public function should_fail_when_give_only_numbers(): void
     {
         $this->expectException(\InvalidArgumentException::class);
-        $textProcessor = new TextProcessing('386491');
+        $textProcessor = new TextProcessing(self::INVALID_TEXT_WITH_ONLY_NUMBERS);
     }
 }
