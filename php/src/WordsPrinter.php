3278c98890aa2a55f6076a37d490018d618045c9
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-18 14:43:42 +0200

    should_print_words_when_give_two_words

--- php/src/WordsPrinter.php
+++ php/src/WordsPrinter.php
@@ -5,8 +5,20 @@ namespace TextProcessingKata;
 class WordsPrinter
 {
 
-    public function print(array $array): string
+    public function print(array $words): string
     {
-        return '1. ' . $array[0];
+        $body = '';
+        $label = 1;
+        foreach ($words as $word) {
+            $body .= $label . '. ' . $word;
+
+            if (sizeof($words) > $label) {
+                $body .= PHP_EOL;
+            }
+
+            $label++;
+        }
+
+        return $body;
     }
 }
--- php/tests/WordsPrinterTest.php
+++ php/tests/WordsPrinterTest.php
@@ -13,4 +13,11 @@ class WordsPrinterTest extends TestCase
         $wordsPrinter = new WordsPrinter();
         self::assertSame($wordsPrinter->print(['one_word']), '1. one_word');
     }
+
+    /** @test */
+    public function should_print_words_when_give_two_words(): void
+    {
+        $wordsPrinter = new WordsPrinter();
+        self::assertSame($wordsPrinter->print(['one_word', 'two_word']), "1. one_word\n2. two_word");
+    }
 }
