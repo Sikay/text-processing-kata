a01ee7cea572e9c74d98fa14e8ea253b67c31c98
Pedro Hernandez-Mora de Fuentes <pedrohdezmora@gmail.com>
2022-05-18 18:13:47 +0200

    should_given_text_return_the_top_words_used_without_code_snippets

--- php/src/TextProcessing.php
+++ php/src/TextProcessing.php
@@ -4,15 +4,20 @@ namespace TextProcessingKata;
 
 class TextProcessing implements Processor
 {
+    private const CLEANER_CODE_SNIPPETS_REGEX = '/`[\s\S]+?`/';
+
     public function analyse(string $text): ProcessText
     {
         $words = explode(' ', $this->clean($text));
         return new ProcessText($this->orderByRepeatWords($words), sizeof($words));
     }
 
-    private function clean(string $text)
+    private function clean(string $text): string
     {
-        return str_replace(['.', ','], '', strtolower($text));
+        $lowerCaseText = strtolower($text);
+        $textWithoutCodeSnippets = preg_replace(self::CLEANER_CODE_SNIPPETS_REGEX, '', $lowerCaseText);
+        $textWithoutSigns = str_replace(['.', ','], '', $textWithoutCodeSnippets);
+        return trim($textWithoutSigns);
     }
 
     private function orderByRepeatWords(array $words): array
--- php/tests/TopWordServiceTest.php
+++ php/tests/TopWordServiceTest.php
@@ -9,28 +9,43 @@ use TextProcessingKata\WordsPrinter;
 
 class TopWordsServiceTest extends TestCase
 {
+    private const VALID_EXPECTED_OUTPUT = "Those are the top 10 words used:" . PHP_EOL .
+                                            PHP_EOL .
+                                            "1. you" . PHP_EOL .
+                                            "2. this" . PHP_EOL .
+                                            "3. hello" . PHP_EOL .
+                                            "4. text" . PHP_EOL .
+                                            "5. test" . PHP_EOL .
+                                            "6. your" . PHP_EOL .
+                                            "7. as" . PHP_EOL .
+                                            "8. it" . PHP_EOL .
+                                            "9. make" . PHP_EOL .
+                                            "10. and" . PHP_EOL .
+                                            PHP_EOL .
+                                            "The text has in total 21 words";
+    private const VALID_TEXT_WITH_SIGNS = "Hello, this is an example for you to practice. You should grab this text and make it as your test case.";
+
     /** @test */
     public function should_given_text_return_the_top_words_used(): void
     {
-        $expected = "Those are the top 10 words used:" . PHP_EOL .
-                    PHP_EOL .
-                    "1. you" . PHP_EOL .
-                    "2. this" . PHP_EOL .
-                    "3. hello" . PHP_EOL .
-                    "4. text" . PHP_EOL .
-                    "5. test" . PHP_EOL .
-                    "6. your" . PHP_EOL .
-                    "7. as" . PHP_EOL .
-                    "8. it" . PHP_EOL .
-                    "9. make" . PHP_EOL .
-                    "10. and" . PHP_EOL .
-                    PHP_EOL .
-                    "The text has in total 21 words";
-
-        $text = "Hello, this is an example for you to practice. You should grab this text and make it as your test case.";
         $textProcessing = new TextProcessing();
         $printer = new WordsPrinter();
         $topWordService = new TopWordsService($textProcessing, $printer);
-        self::assertSame($expected, $topWordService->execute($text));
+        self::assertSame(self::VALID_EXPECTED_OUTPUT, $topWordService->execute(self::VALID_TEXT_WITH_SIGNS));
+    }
+
+    /** @test */
+    public function should_given_text_return_the_top_words_used_without_code_snippets(): void
+    {
+        $text = "Hello, this is an example for you to practice. You should grab this text and make it as your test case:" .
+                    "` ` `javascript" .
+                    "if (true) {" .
+                    "  console.log('should should should')" .
+                    "}" .
+                    "` ` `";
+        $textProcessing = new TextProcessing();
+        $printer = new WordsPrinter();
+        $topWordService = new TopWordsService($textProcessing, $printer);
+        self::assertSame(self::VALID_EXPECTED_OUTPUT, $topWordService->execute($text));
     }
 }
